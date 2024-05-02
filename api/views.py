127b788b27fe204f6f90cc606d51900672b3d0a7
Muhammad Rizki Malik Aziz <rizkimalik221001@gmail.com>
2024-05-02 08:34:20 +0700

    Initial Commit feat: Add Registration view

--- api/views.py
+++ api/views.py
@@ -3,11 +3,9 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
 
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-    authentication_classes = [TokenAuthentication]
